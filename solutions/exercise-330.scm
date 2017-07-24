da550da78fc8ba6288e92d99a122273316e91fe9
devmacrile <devin@farmlogs.com>
2017-07-24 16:44:13 -0400

    3.30 fix

--- solutions/exercise-330.scm
+++ solutions/exercise-330.scm
@@ -1,12 +1,14 @@
-(define (ripple-carry-adder A B S c-in)
-    (if (null? (car A))
-        `ok
-        (let ((a (car A))
-              (b (car B))
-              (s (car S))
-              (c-out (make-wire)))
-          (full-adder a b c-in s c-out)
-          (ripply-carry-adder (cdr A) (cdr B) (cdr S) c-out))))
+(define (ripple-carry-adder A B S c-out)
+    (let ((a (car A)
+          (b (car B))
+          (s (car S)) 
+          (c-in (make-wire)))
+        (if (null? (cdr A))
+            (set-signal! c-in 0)
+            (ripple-carry-adder (cdr A) (cdr B) (cdr S) ))
+        (full-adder a b c-in s c-out))))
+; edit: misunderstood the diagram - need to recurse to the nth binary
+; digit and bring the carry back from there.
 
 ; Delay in terms of delays for and-gates, or-gates, inverters?
 ; Each full-adder will have two half-adder delays and an or delay.
